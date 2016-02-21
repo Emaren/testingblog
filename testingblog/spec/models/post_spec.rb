0d556dddc7dad869cbbd5a7faf8fee6bdda64526
Tony Blum <tonyblum@me.com>
2016-02-20 17:51:52 -0800

    add second failing test

--- testingblog/spec/models/post_spec.rb
+++ testingblog/spec/models/post_spec.rb
@@ -9,5 +9,8 @@ RSpec.describe Post, type: :model do
   it "should have a matching body" do
     expect(@post.body).to eq("My Body")
   end
+  it "should have a matching title" do
+    expect(@post.title).to eq("Your Title")
+  end
 
 end
