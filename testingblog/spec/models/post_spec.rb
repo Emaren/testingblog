55d871f55fee75c5fc1c47cf8b68722be20cab12
Tony Blum <tonyblum@me.com>
2016-02-20 17:46:03 -0800

    create model and first failing test

---
+++ testingblog/spec/models/post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  before(:all) do
+    @post = Post.new(body: "My Body", title: "My Title")
+  end
+
+  it "should have a matching body" do
+    expect(@post.body).to eq("Your Body")
+  end
+
+end
